b23f4e4f603471b04e347b92806e9b4a73713bc7
khanofficial012w-ship-it <khanofficial012w@gmail.com>
2026-02-18 16:59:10 +0500

    user and audit models created

---
+++ models/product.model.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({}, { timestamps: true });
+
+module.exports = mongoose.model("Product", productSchema);
---
+++ models/user.model.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username must be required"],
+      lowercase: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 30,
+      unique: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Invalid email format"],
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      enum: ["active", "suspended"],
+      default: "active",
+      index: true,
+    },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) return next();
+
+    const saltRounds = Number(process.env.SALT_ROUNDS || 10);
+    this.password = await bcrypt.hash(this.password, saltRounds);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
