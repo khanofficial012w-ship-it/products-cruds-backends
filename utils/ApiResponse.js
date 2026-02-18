0a9d16081aa0358de5834b967283ba0a327baf53
khanofficial012w-ship-it <khanofficial012w@gmail.com>
2026-02-19 01:48:57 +0500

    error-response-handling-utils added

---
+++ utils/ApiResponse.js
@@ -0,0 +1,16 @@
+class ApiResponse {
+  constructor({
+    statusCode = 200,
+    message = "Success",
+    data = null,
+    meta = null,
+  }) {
+    this.statusCode = statusCode;
+    this.success = statusCode < 400;
+    this.message = message;
+    this.data = data;
+    if (meta) this.meta = meta;
+  }
+}
+
+module.exports = ApiResponse;
